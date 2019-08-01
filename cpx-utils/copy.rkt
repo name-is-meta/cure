28015e4ed48679b7fe055366abed3e84160f1240
Abraham Brownell <abrahambrownell@gmail.com>
2019-08-01 09:41:29 -0400

    Log. Copy.rkt.

--- cpx-utils/copy.rkt
+++ cpx-utils/copy.rkt
@@ -1,10 +1,32 @@
 #lang racket
-(require htdp/gui)
+(require racket/gui)
 
+(define (build-gui)
+  
+  (define f (new frame%
+                 [label "The Frame"]
+                 [width 400]
+                 [height 300]))
+
+  (define p (new panel%
+                 (parent f)))
+  
+  (define b (new button%
+                 [label "Copy Things"]
+                 [parent p]
+                 [callback button-callback]
+                 ))
+
+  ;; Return the parent
+  f)
 
 (define desk_dir (find-system-path 'desk-dir))
 (define source (build-path desk_dir "test_in"))
 (define dest (build-path desk_dir "test_out"))
+
+(define (button-callback obj evt)
+  (copy-file source dest)
+  (send b set-label "Copied!"))
 
 (define w
   (create-window
